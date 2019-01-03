8d31dbf73635da39cf1d5ef36bb5a59a34ca2680
Tersoo Atsen <tersoo.atsen@outlook.com>
2019-01-03 02:20:29 +0100

    ch(react): setup react project
    - install modules
    - configure babel
    - configure webpack
    
    [Finishes #162920713]

---
+++ test/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../src/App';
+
+test('App snapshot test', () => {
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
+});
