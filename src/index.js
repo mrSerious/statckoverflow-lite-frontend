94eea951d16463e57cbdbf4bbbaf7d26e4ba8c82
Tersoo Atsen <tersoo.atsen@outlook.com>
2019-01-03 02:20:29 +0100

    ch(react): setup react project
    - install modules
    - configure babel
    - configure webpack
    
    [Finishes #162920713]

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import '../public/styles/index.scss';
+
+import App from './App';
+
+render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins() {
+                return [
+                  precss,
+                  autoprefixer
+                ];
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public', 'index.html'),
+    port: 8080,
+    historyApiFallback: true,
+    hot: true
+  },
+});
