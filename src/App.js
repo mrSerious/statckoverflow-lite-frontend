5fde2aa6319f717ea4a8fe5dd1f0375690bcfcb0
Tersoo Atsen <tersoo.atsen@outlook.com>
2019-01-05 00:58:45 +0100

    ch(react-router): setup-react-router
    - install modules
    - configure react router
    
    [Finishes #162920864]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store/store';
+import NotFound from './components/404/NotFound';
+import routes from './routes';
 
 class App extends Component {
   constructor(props) {
@@ -13,9 +16,23 @@ class App extends Component {
     return (
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <Fragment>
-            <h1>Hello</h1>
-          </Fragment>
+          <Router>
+            <Fragment>
+              <Switch>
+                {
+                  routes.map(route => (
+                    <Route
+                      exact={route.exact}
+                      path={route.path}
+                      key={route.path}
+                      component={route.component}
+                      />
+                  ))
+                }
+                <Route component={NotFound} />
+              </Switch>
+            </Fragment>
+          </Router>
         </PersistGate>
       </Provider>
     );
--- test/src/App.test.js
+++ test/src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import App from '../src/App';
+import App from '../../src/App';
 
 test('App snapshot test', () => {
   const component = shallow(<App />);
